d46d3e8d7d2ba9ec946eb3bf2860983ad2367a78
xerxes1986 <xerxes1986@gmail.com>
2013-03-22 16:17:56 -0500

    fixed installCMSSW cvs login

--- Driver/installCMSSW.py
+++ Driver/installCMSSW.py
@@ -5,7 +5,8 @@ os.environ["SCRAM_ARCH"] = "slc5_amd64_gcc462"
 call(["scram","project","CMSSW","CMSSW_5_3_3"])
 os.chdir("CMSSW_5_3_3/src")
 os.system("eval `scramv1 runtime -sh`")
-os.environ["CVSROOT"] = ":pserver:anonymous@cmssw.cvs.cern.ch:/local/reps/CMSSW"
+os.environ["CVSROOT"] = ":pserver:anonymous:98passwd@cmssw.cvs.cern.ch:/local/reps/CMSSW"
+os.system("cvs login")
 call(["cvs","co","-d","UHHAnalysis/NtupleWriter","-r","Dec-05-2012-v1","UserCode/UHHAnalysis/NtupleWriter"])
-call(["cvs","co","-d","EGamma/EGamma/AnalysisTools","-r","V00-00-08","UserCode/EGamma/EGammaAnalysisTools"])
+call(["cvs","co","-d","EGamma/EGammaAnalysisTools","-r","V00-00-08","UserCode/EGamma/EGammaAnalysisTools"])
 call(["scram","b","-j","10"])
